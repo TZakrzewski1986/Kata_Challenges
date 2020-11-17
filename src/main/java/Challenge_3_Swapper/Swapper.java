93df3fbd4b1dc72c41a734000560276ccf3b6415
Tomasz Zakrzewski <tomasz.zakrzewski86@gmail.com>
2020-11-17 15:20:12 +0100

    17.11.2020

---
+++ src/main/java/Challenge_3_Swapper/Swapper.java
@@ -0,0 +1,16 @@
+package Challenge_3_Swapper;
+
+public class Swapper {
+
+    public Object[] arguments;
+
+    public Swapper(final Object[] args){
+        arguments=args;
+    }
+
+    public void swapValues() {
+        Object temp = arguments[0];
+        arguments[0] = arguments[1];
+        arguments[1] = temp;
+    }
+}
---
+++ src/main/java/Challenge_3_Swapper/SwapperRunner.java
@@ -0,0 +1,11 @@
+package Challenge_3_Swapper;
+
+import java.util.Arrays;
+
+public class SwapperRunner {
+    public static void main(String[] args) {
+        Swapper swapper = new Swapper(args);
+        swapper.swapValues();
+
+    }
+}
