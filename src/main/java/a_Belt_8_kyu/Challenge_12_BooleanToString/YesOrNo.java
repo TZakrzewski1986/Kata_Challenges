145e4cf8a6d22001045e5a3985d4057695a2d8a4
Tomasz Zakrzewski <tomasz.zakrzewski86@gmail.com>
2020-12-08 12:17:09 +0100

    08.12.2020 vel 2

---
+++ src/main/java/a_Belt_8_kyu/Challenge_12_BooleanToString/YesOrNo.java
@@ -0,0 +1,11 @@
+package a_Belt_8_kyu.Challenge_12_BooleanToString;
+
+public class YesOrNo {
+    public static String boolToWord(boolean b) {
+        if (b){
+         return "Yes";
+        } else {
+            return "No";
+        }
+    }
+}
---
+++ src/main/java/a_Belt_8_kyu/Challenge_12_BooleanToString/YesOrNoRunner.java
@@ -0,0 +1,8 @@
+package a_Belt_8_kyu.Challenge_12_BooleanToString;
+
+public class YesOrNoRunner {
+    public static void main(String[] args) {
+        System.out.println(YesOrNo.boolToWord(false));
+
+    }
+}
